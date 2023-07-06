8feb82aabb54933598d03cd5a9bcbcc439a1e4b8
sultaale <impa1387@gmail.com>
2023-07-06 13:57:31 +0300

    Added greetings

--- GroupJavaStudy/src/Main.java
+++ GroupJavaStudy/src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         //El Baron
         System.out.println("El Baron was here");
+        //sultaale
+        System.out.println("Hello, everybody!");
     }
 }
